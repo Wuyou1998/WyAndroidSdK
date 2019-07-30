702a2e5e1f1990dc3b196c01c748d4b800dd2560
wuyou1998 <1020131041@qq.com>
2019-07-30 14:40:46 +0800

    集成zxing二维码扫描

--- app/src/main/java/com/wy/wyandroidsdk/MainActivity.java
+++ app/src/main/java/com/wy/wyandroidsdk/MainActivity.java
@@ -1,8 +1,19 @@
 package com.wy.wyandroidsdk;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.wy.wyandroidsdk.zxing.app.CaptureActivity;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +23,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        checkPermissions();
+        initView();
+    }
 
+    private void initView() {
+        findViewById(R.id.tv_text).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivityForResult(new Intent(MainActivity.this, CaptureActivity.class),1);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Toast.makeText(this,data.getStringExtra("SCAN_RESULT"),Toast.LENGTH_SHORT).show();
+    }
+
+    private void checkPermissions() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.CAMERA},1);
+        }
     }
 }
